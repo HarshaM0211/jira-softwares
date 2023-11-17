286d9c4a21c1180b15e19b11290946cb437e2709
harsha <mukthapuramharsha@gmail.com>
2023-11-17 09:08:20 +0530

    [Bean and DTO] Added bean and dto for organisation

---
+++ src/main/java/com/mandark/jira/app/beans/OrganisationBean.java
@@ -0,0 +1,56 @@
+package com.mandark.jira.app.beans;
+
+import com.mandark.jira.app.persistence.orm.entity.Organisation;
+import com.mandark.jira.spi.app.EntityBean;
+
+
+public class OrganisationBean implements EntityBean<Organisation> {
+
+    // Fields
+    // ------------------------------------------------------------------------
+
+    private String name;
+
+    private String description;
+
+    // Constructors
+    // ------------------------------------------------------------------------
+    
+    
+    public OrganisationBean() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+    
+    public OrganisationBean(String name, String description) {
+        super();
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    // ------------------------------------------------------------------------
+
+
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
