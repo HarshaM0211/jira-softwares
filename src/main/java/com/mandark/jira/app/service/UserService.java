b2eccd4a01457e0e2b15c9b506ef48f0c7d00f83
harsha <mukthapuramharsha@gmail.com>
2023-12-21 17:14:16 +0530

    [isUserInProject] Added new Service Method in UserService Classes

--- src/main/java/com/mandark/jira/app/service/UserService.java
+++ src/main/java/com/mandark/jira/app/service/UserService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.mandark.jira.app.beans.UserBean;
 import com.mandark.jira.app.dto.UserDTO;
+import com.mandark.jira.app.persistence.orm.entity.Project;
 import com.mandark.jira.app.persistence.orm.entity.User;
+import com.mandark.jira.spi.app.query.Criteria;
 import com.mandark.jira.spi.app.service.EntityService;
 
 
@@ -23,4 +25,8 @@ public interface UserService extends EntityService<Integer, User, UserDTO> {
     int count(final Integer orgId);
 
     boolean isUserInOrg(final Integer userId, final Integer orgId);
+
+    boolean isUserInProject(final User userEntity, final Project projectEntity);
+
+    Criteria getOrgCriteria(final Integer orgId);
 }
